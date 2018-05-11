e32e72391d68c0ea1db08ec3962349e8505fc315
Mitchell Richeson <mricheson@dminc.com>
2018-05-11 14:11:08 -0400

    CORS Support

--- src/main/java/com/windmill/api/repository/AccountTypeRepository.java
+++ src/main/java/com/windmill/api/repository/AccountTypeRepository.java
@@ -2,9 +2,10 @@ package com.windmill.api.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.windmill.api.model.AccountType;
-
+@CrossOrigin
 @RepositoryRestController
 public interface AccountTypeRepository extends JpaRepository<AccountType, Long>{
 
--- src/main/java/com/windmill/api/repository/InstitutionRepository.java
+++ src/main/java/com/windmill/api/repository/InstitutionRepository.java
@@ -2,10 +2,12 @@ package com.windmill.api.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
-import com.windmill.api.model.Institution; 
+import com.windmill.api.model.Institution;
 
+@CrossOrigin
 @RepositoryRestController
-public interface InstitutionRepository extends JpaRepository<Institution, Long>{
+public interface InstitutionRepository extends JpaRepository<Institution, Long> {
 
 }
