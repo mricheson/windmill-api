cc5d0907ae09d97d3189ef822f126ab0518e8f77
Mitchell Richeson <mricheson@gmail.com>
2020-04-19 10:43:40 -0400

    Authorized Redirect URL

--- src/main/java/org/windmill/api/security/oauth2/OAuth2AuthenticationSuccessHandler.java
+++ src/main/java/org/windmill/api/security/oauth2/OAuth2AuthenticationSuccessHandler.java
@@ -76,15 +76,13 @@ public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationS
 	private boolean isAuthorizedRedirectUri(String uri) {
 		URI clientRedirectUri = URI.create(uri);
 
-		return appProperties.getOauth2().getAuthorizedRedirectUris().stream().anyMatch(authorizedRedirectUri -> {
-			// Only validate host and port. Let the clients use different paths if they want
-			// to
-			URI authorizedURI = URI.create(authorizedRedirectUri);
-			if (authorizedURI.getHost().equalsIgnoreCase(clientRedirectUri.getHost())
-					&& authorizedURI.getPort() == clientRedirectUri.getPort()) {
-				return true;
-			}
-			return false;
-		});
+		// Only validate host and port. Let the clients use different paths if they want
+		// to
+		URI authorizedURI = URI.create(appProperties.getOauth2().getAuthorizedRedirect());
+		if (authorizedURI.getHost().equalsIgnoreCase(clientRedirectUri.getHost())
+				&& authorizedURI.getPort() == clientRedirectUri.getPort()) {
+			return true;
+		}
+		return false;
 	}
 }
